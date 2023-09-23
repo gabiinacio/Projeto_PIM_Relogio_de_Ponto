3f71a2fd261b2ab76da8522f35b2ebbe3e903e67
gabiinacio <gabriellepi8@gmail.com>
2023-09-23 17:13:58 -0300

    Versão 1.1
    Atualizações:
    > Tela de relogio adicionada;
    > Logo adicionada;
    > Desing melhorado;

---
+++ app/src/main/java/com/example/relogiodeponto/relogioPonto.java
@@ -0,0 +1,13 @@
+package com.example.relogiodeponto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class relogioPonto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relogio_ponto);
+    }
+}
