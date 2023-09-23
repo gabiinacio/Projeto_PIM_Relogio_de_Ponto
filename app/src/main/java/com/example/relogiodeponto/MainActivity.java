9de975def1f8d694216727092e38bf79681d79f1
Jennifer Inacio <jenniferp.i8@gmail.com>
2023-09-23 16:02:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/relogiodeponto/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.relogiodeponto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public class Configuracao
+    {
+
+    }
+
+    public class Eventos
+    {
+
+    }
+}
